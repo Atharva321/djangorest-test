a815a01c6bda07bf2d7bdcb9e36cc3752b21f620
Atharva321 <atharvapatil19202@gmail.com>
2022-05-02 18:14:22 +0000

    importing p[aackages

--- djangoresttest/testapp/urls.py
+++ djangoresttest/testapp/urls.py
@@ -1,3 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from testapp_api import urls as testapp_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
